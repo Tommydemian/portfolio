e7fb91da34ca4eee01a7c296605cd460854bb43b
Tomas <tomas@getcharly.com>
2023-04-03 07:25:30 -0300

    retry push

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -7,7 +7,7 @@ const Courses = () => {
     <div className='my-20'>
     <Title>Further Education</Title>
     <p>Honestly I have thousands of studying hours but are probably the most in depth courses that I took. In fact both articles are inspired in this courses.</p>
-    <Carousel />
+    {/* <Carousel /> */}
     </div>
   )
 }
